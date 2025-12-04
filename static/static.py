d6ffb2210bba8fae7f869e7f54c3911f9ed2d6dd
GarellBale <garello.andrea2@yahoo.it>
2025-12-04 19:37:41 +0100

    Update static.py

--- static/static.py
+++ static/static.py
@@ -1,5 +1,17 @@
 HTML = """
-The airplane went through such bad turbulence that everyone thought they were going to die.
-You find people expressing many different opinions.
-What a big truck!
+We should all be sure to keep our promises, even when it is difficult.
+People don't realize how often you can fail as a writer, even after you get a good job or get a book deal.
+Her great-great-great grandfather was an outlaw in the Old West.
+They were short staffed, so it took over an hour for her to get her burrito.
+That is a pencil.
+She really wanted to go out to a diner and eat some greasy toast and eggs for breakfast.
+Moral Foundations Theory states that there are five main moral foundations which inform people’s moral decision-making.
+The kid had made it his mission to pee in every pool he swam in.
+I was called into the office first.
+I'd like to look at some sweaters.
+It was raining so badly that she skidded on the pavement.
+She decided to take the bus because it was supposed to be quicker, but then it ended up being twenty minutes late.
+Have you opened the door?
+Is Okayama a big city?
+How's it going now that you're all alone?
 """
